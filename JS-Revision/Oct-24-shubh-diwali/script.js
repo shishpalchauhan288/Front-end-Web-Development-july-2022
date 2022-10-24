6821f9a12e8491466fdaa7ec994ab735c3c30c81
shishpal <shishpalchauhan288@gail.com>
2022-10-24 16:17:26 +0530

    happy diwali

---
+++ JS-Revision/Oct-24-shubh-diwali/script.js
@@ -0,0 +1,29 @@
+
+var jashn_A_Diwali_wish_Message_By_SHISHPAL_CHAUHAN = prompt("Enter a Day: between 22 to 26  ");
+if( jashn_A_Diwali_wish_Message_By_SHISHPAL_CHAUHAN == 22) {
+    console.log("Happy Dhanteras 🎁📦🥳🤴");
+}
+
+else if (jashn_A_Diwali_wish_Message_By_SHISHPAL_CHAUHAN == 23) {
+  console.log(" Happy Chhoti Diwali 💫❤️🌟🔆");
+}
+
+else if (jashn_A_Diwali_wish_Message_By_SHISHPAL_CHAUHAN == 24) {
+    console.log(" Happy Diwali 💫🌟⁕🎆🎇✨🎉🎊🎀🪄");
+  }
+
+  else if (jashn_A_Diwali_wish_Message_By_SHISHPAL_CHAUHAN == 25) {
+    console.log(" Happy Govardhan Pooja 🕯️🪔💡✉️🍏");
+  }
+
+  else if (jashn_A_Diwali_wish_Message_By_SHISHPAL_CHAUHAN == 26) {
+    console.log(" Happy Bhaiya Dooj 🎀🎗️");
+  }
+
+else {
+console.log("Festive season is over now concentrate on your work Eat, Sleep  Code & Repeat Again 🕑💱💲🔙🔜♻️💯🖥️💻")
+
+    
+}
+
+    
